ea5bc516c1cea805f9640c7fd968e6ea168da141
David Claveau <daveclaveau@gmail.com>
2021-05-05 20:01:05 +0000

    Added stdin functionality to allow user to enter from the command line. Added ability to exit function.

--- play.js
+++ play.js
@@ -1,5 +1,36 @@
 const net = require('net');
 const { connect } = require('./client')
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = (key) =>  {
+    if (key === '\u0003') {
+      console.log('You have left the game. Goodbye!');
+      process.exit();
+    }
+    // if (key === '\033[A') {
+    //   console.log('You have pressed the up key');
+    // }
+    // if (key === '\033[B') {
+    //   console.log('You have pressed the down key');
+    // }
+    // if (key === '\033[D') {
+    //   console.log('You have pressed the left key');
+    // }
+    // if (key === '\033[C') {
+    //   console.log('You have pressed the right key');
+    // }
+  }
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
 console.log('Connecting...');
 connect();
+setupInput();
