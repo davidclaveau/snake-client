3f49ea84ac8b7c5c876cd837ad9d4f2976af8ada
David Claveau <daveclaveau@gmail.com>
2021-05-05 20:30:15 +0000

    Added functionality to move the snake using WASD. Receive an object from client.js and pass that to setupInput to send directions to the server.

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,18 +12,18 @@ const setupInput = function() {
       console.log('You have left the game. Goodbye!');
       process.exit();
     }
-    // if (key === '\033[A') {
-    //   console.log('You have pressed the up key');
-    // }
-    // if (key === '\033[B') {
-    //   console.log('You have pressed the down key');
-    // }
-    // if (key === '\033[D') {
-    //   console.log('You have pressed the left key');
-    // }
-    // if (key === '\033[C') {
-    //   console.log('You have pressed the right key');
-    // }
+    if (key === 'w') {
+      connection.write('Move: up');
+    }
+    if (key === 's') {
+      connection.write('Move: down');
+    }
+    if (key === 'a') {
+      connection.write('Move: left');
+    }
+    if (key === 'd') {
+      connection.write('Move: right');
+    }
   };
 
   stdin.on('data', handleUserInput);
