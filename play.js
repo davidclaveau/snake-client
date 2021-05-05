797083691dbe386d2f3c1da75db018c19db72d7d
David Claveau <daveclaveau@gmail.com>
2021-05-05 19:25:54 +0000

    Added client.js which takes the connect funciton for the client. Exported the connect function to play.js instead and removed duplicate code.

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const net = require('net');
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
-  });
-
-  conn.setEncoding('utf8');
-
-  conn.on("connect", (data) => {
-    console.log('Successfully connected to the server');
-  });
-  
-  conn.on("data", (message) => {
-    console.log("Server says:", message);
-  });
-
-  return conn;
-};
+const { connect } = require('./client')
 
 console.log('Connecting...');
 connect();
